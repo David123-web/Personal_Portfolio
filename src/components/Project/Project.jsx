720a9e0d6c19609aa1e8ddde952f83a4ee393317
David123-web <69841889+David123-web@users.noreply.github.com>
2024-11-07 22:37:21 -0800

    Modified the project section

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,22 +1,50 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import "swiper/css/effect-coverflow"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+import { Navigation, Autoplay, EffectCoverflow } from "swiper/modules";
 import styles from "./Project.module.css"
 import projects from '../../data/projects.json'
 import { ProjectCard } from './ProjectCard'
+
+
+
 export const Project = () => {
   return (
     <section className={styles.container} id='projects'>
         <h2 className={styles.title}>Projects</h2>
-        <div className={styles.projects}>
+        {/*<div className={styles.scrollButton} onClick={scrollLeft}>◀</div>*/}
+
+        <Swiper
+            effect={'coverflow'}
+            grabCursor ={ true }
+            centeredSlides = {true}
+            loop = {true}
+            slidesPerView = {3}
+            spaceBetween={50}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            modules={[Autoplay, EffectCoverflow, Navigation]}
+            className={styles.projects}
+        >
             {
                 projects.map((project, id)=>{
 
                     return (
-                    <ProjectCard key={id} project={project}/>
+                      <SwiperSlide key={id}>
+                        <ProjectCard key={id} project={project}/>
+                      </SwiperSlide>
+
                     )
 
                 })
             }
-        </div>
+        </Swiper>
+       
+        {/*<div className={styles.scrollButton} onClick={scrollRight}>▶</div>*/}
     </section>
   )
 }
